ee112d96913bceef71ba0a95f45e104952841458
Your Name <shado@LAPTOP-U7GG9IKM>
2024-11-04 18:15:55 +0800

    initial commit

---
+++ userProfile.js
@@ -0,0 +1,8 @@
+let firstName = "Tyler";
+let lastName = "Smith";
+fullName = firstName + " " + lastName;
+let age = 21;
+let favNum = 7;
+let favColor = "Green";
+
+console.log(fullName, age, favNum, favColor)
